89f3b3af9cae80515b2ed9e131dc0ece471285ed
beverley-dick <beverleydick11@gmail.com>
2023-05-08 16:59:50 +0300

    Add project files.

---
+++ ThisKeyw/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ThisKeyw
+{
+    class Student
+    {
+        private string[] name = new string[3];
+
+        //declaring an indexer
+        public string this[int index]
+        {
+            // returns value of array element
+            get
+            {
+                return name[index];
+            }
+
+            //sets value of array element
+            set
+            {
+                name[index] = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            Student s1 = new Student();
+            s1[0] = "Ram";
+            s1[1] = "Shyam";
+            s1[2] = "Gopal";
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(s1[i] + " ");
+            }
+        }
+    }
+}
